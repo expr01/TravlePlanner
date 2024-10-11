565f8c6d11ed95c98e4cac3e2c54f08380ada97d
expr01 <jeje300011@gmail.com>
2024-10-12 00:48:44 +0900

    여행 계획 목록 리스팅(Read) 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+// main.dart
 import 'package:flutter/material.dart';
+import 'travle_plan/pages/travel_plan_list_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,14 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter App'),
-        ),
-        body: Center(
-          child: Text('Hello, World!'),
-        ),
+      title: 'Travel Planner',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.grey[200],
       ),
+      home: TravelPlanListPage(),
+      debugShowCheckedModeBanner: false, // 디버그 배너 제거
     );
   }
 }
